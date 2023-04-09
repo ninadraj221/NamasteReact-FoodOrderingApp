a9b0bfbb8f13c78472c3fdc09ae001201247f8f7
ninadraj221 <ninadraj321@gmail.com>
2023-04-09 15:12:36 +0530

    Lifecyle of Functional Component Inside About Us

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,5 +1,23 @@
+import { useEffect, useState } from "react";
+
 const ContactUs = () => {
-  return <h1>Contact Us</h1>;
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log("Inside useEffect Contact Us");
+    setCount(1);
+  }, []);
+
+  console.log("Render Contact Us");
+  console.log(count);
+
+  return (
+    <div>
+      {console.log("Component is Mounted", count)}
+      <h1>Contact Us</h1>
+      {count}
+    </div>
+  );
 };
 
 export default ContactUs;
